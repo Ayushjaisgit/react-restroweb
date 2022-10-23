5097d1d7f1dc821f08d7693eebd75212369281c8
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-23 18:55:49 +0530

    Added More components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Resturant from './components/Basics/Resturaunt';
 
 function App() {
   return (
     <div className="App">
-     
+      <Resturant/>
     </div>
   );
 }
